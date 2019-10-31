a77c1aa0576c6e3895a7bf292d5946332fb8dbef
John McNerney <42980481+jmacmcnerney@users.noreply.github.com>
2019-10-31 14:22:13 -0400

    Add files via upload
    Added help button overlay information and bad login redirect.

---
+++ login.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var mustache = require('../common/mustache')
+var router = express.Router();
+
+/* GET login page */
+router.get('/', function (req, res, next) {
+	res.render('base_template', {
+		title: 'Login',
+		body: mustache.render('login')
+	})
+})
+
+/* POST login page */
+router.post('/', (req, res, next) => {
+	// hard coded username and password
+	if (req.body.username === 'user' && req.body.password === 'password') {
+		res.redirect(302, '/course/')
+	} else {
+		//res.redirect(302, '/login/?error')
+		//Added by John McNerney
+		res.send("Please enter a valid username and password")
+	}
+})
+
+module.exports = router;
